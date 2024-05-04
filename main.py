bf76fb44d7bb2984db8df145a8e66f0210bcefc0
Nasenboi <c.boendgen@gmail.com>
2024-05-04 17:06:12 +0200

    update logger settings

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import package
 
 if __name__ == "__main__":
 
-    package.globals.LOGGER.info("Info")
     package.globals.LOGGER.debug("Debug")
+    package.globals.LOGGER.info("Info")
     package.globals.LOGGER.error("Error")
     package.globals.LOGGER.warning("Warning")
     package.globals.LOGGER.critical("Critical")
