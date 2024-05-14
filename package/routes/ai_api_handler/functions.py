4216f9b0d079ac077acae45566f2b12887b7674b
Nasenboi <c.boendgen@gmail.com>
2024-05-14 09:58:00 +0200

    header

--- package/routes/ai_api_handler/functions.py
+++ package/routes/ai_api_handler/functions.py
@@ -1,6 +1,6 @@
 '''########################################################################################
 Name: ai_api_handler/functions.py
-Description: This file contains the functions for the AiApiHandler class in ai_api_handler.py
+Description: 
 Imports:
 '''
 
--- package/routes/ai_api_handler/routes.py
+++ package/routes/ai_api_handler/routes.py
@@ -1,6 +1,6 @@
 '''########################################################################################
 Name: ai_api_handler/routes.py
-Description: This file contains the possible api routes and calls for the ai api handler
+Description: 
 Imports:
 '''
 
