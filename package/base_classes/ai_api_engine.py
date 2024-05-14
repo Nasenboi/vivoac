e586a76517c39a9b8337b399e2b690f9f2928cfc
Nasenboi <c.boendgen@gmail.com>
2024-05-14 10:26:47 +0200

    add class bone

--- package/base_classes/ai_api_engine.py
+++ package/base_classes/ai_api_engine.py
@@ -3,6 +3,15 @@ Name: ai_api_engine.py
 Description: 
 Imports:
 '''
-
+from ..utils import virtual
 '''
 ########################################################################################'''
+
+class AI_API_Engine():
+    # class variables:
+    token = None
+    base_url = None
+
+    def __init__(self):
+        pass
+
