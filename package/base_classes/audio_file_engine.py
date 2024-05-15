09207ff9f402f28ed25b0ed5584b77ca2a682e1f
Nasenboi <c.boendgen@gmail.com>
2024-05-15 10:31:29 +0200

    added base class engine functions

--- package/base_classes/audio_file_engine.py
+++ package/base_classes/audio_file_engine.py
@@ -3,6 +3,41 @@ Name: audio_file_engine.py
 Description: 
 Imports:
 '''
-
+from ..utils.decorators import virtual
+from typing import List, Union
+from ..routes.audio.models import Audio_Format
 '''
 ########################################################################################'''
+
+
+class Audio_File_Engine():
+    # class variables:
+    target_format: Audio_Format = None
+
+    def __init__(self, target_format: Audio_Format = None):
+        self.target_format = target_format
+
+
+    ############################################################
+    # getter functions:
+
+
+    @virtual
+    def get_audio_files(self, file_paths = Union[List[str], str]) -> Union[List[bytes], bytes]:
+        pass
+
+    ############################################################
+    # Audio file functions:
+
+    @virtual
+    def save_audio_file(self, file_path: str, audio_data: bytes) -> None:
+        pass
+
+
+    @virtual
+    def delete_audio_file(self, file_path: str) -> None:
+        pass
+
+    @virtual
+    def standardize_audio_file(self, file_path: str) -> bytes:
+        pass
--- package/base_classes/script_db_engine.py
+++ package/base_classes/script_db_engine.py
@@ -3,6 +3,22 @@ Name: script_db_engine.py
 Description: 
 Imports:
 '''
-
+from ..routes.script.models import Script
+from ..utils.decorators import virtual
+from typing import List, Union
 '''
 ########################################################################################'''
+
+class Script_DB_Engine():
+    # class variables:
+
+    def __init__(self):
+        pass
+
+
+    ############################################################
+    # getter functions:
+
+    @virtual
+    def get_script_lines(self, script: Script = Script()) -> Union[List[Script], Script]:
+        pass
