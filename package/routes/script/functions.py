c96c16921a017574631b205e8cb27194b6c14eea
Nasenboi <c.boendgen@gmail.com>
2024-05-16 16:24:44 +0200

    no assert on empty body

--- package/routes/script/functions.py
+++ package/routes/script/functions.py
@@ -17,7 +17,3 @@ from .models import *
 
 def get_script_lines(script: Script) -> Union[List[Union[Script]], Script]:
     LOGGER.debug(f"Getting script: {script}")
-
-    if script.is_empty():
-        LOGGER.warning(f"Recieved an empty script")
-        raise HTTPException(status_code=400, detail="Recieved an empty script")
