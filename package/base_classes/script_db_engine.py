84810b8a1a47e6f6c48f8843e8911a8b1dde6597
Nasenboi <c.boendgen@gmail.com>
2024-05-16 12:37:53 +0200

    add some files to modules.py

--- package/base_classes/script_db_engine.py
+++ package/base_classes/script_db_engine.py
@@ -3,7 +3,7 @@ Name: script_db_engine.py
 Description: 
 Imports:
 '''
-from ..routes.script.models import Script
+from ..routes.script.models import Script, Character_Info
 from ..utils.decorators import virtual
 from typing import List, Union
 '''
@@ -11,14 +11,22 @@ from typing import List, Union
 
 class Script_DB_Engine():
     # class variables:
+    script_name: str = None
 
-    def __init__(self):
+    def __init__(self, script_name: str = None):
         pass
-
 
     ############################################################
     # getter functions:
 
     @virtual
+    def get_script_names(self) -> List[str]:
+        pass
+
+    @virtual
     def get_script_lines(self, script: Script = Script()) -> Union[List[Script], Script]:
+        pass
+
+    @virtual
+    def get_character_infos(self, character_info: Character_Info = Character_Info()) -> Union[List[Character_Info], Character_Info]:
         pass
--- package/routes/script/models.py
+++ package/routes/script/models.py
@@ -8,6 +8,15 @@ from typing import Optional, Union
 '''
 ########################################################################################'''
 
+# character into model
+class Character_Info(BaseModel):
+    id: Optional[Union[int, str]]
+    character_name: Optional[str]
+    voice_talent: Optional[str]
+    script_name: Optional[str]
+    number_of_lines: Optional[int]
+    gender: Optional[str]
+
 # script model
 class Script(BaseModel):
     id: Optional[Union[int, str]]
@@ -15,7 +24,7 @@ class Script(BaseModel):
     translation: Optional[str]
     time_restriction: Optional[Union[str, int]]
     voice_talent: Optional[str]
-    character: Optional[str]
+    character_name: Optional[str]
     reference_audio_path: Optional[str]
     delivery_audio_path: Optional[str]
     generatied_audio_path: Optional[str]
