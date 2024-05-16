ad93a15e20af1e378bbf05aea43f8fd4f79f2033
Nasenboi <c.boendgen@gmail.com>
2024-05-16 12:44:44 +0200

    update modules

--- modules/script_db_engine/excel_script_db_engine.py
+++ modules/script_db_engine/excel_script_db_engine.py
@@ -3,30 +3,40 @@ Name: script_db_engine.py
 Description: 
 Imports:
 '''
-from ..routes.script.models import Script, Character_Info
-from ..utils.decorators import virtual
+from ...package.base_classes.script_db_engine import Script_DB_Engine
+from ...package.routes.script.models import Script, Character_Info
 from typing import List, Union
+import panads as pd
 '''
 ########################################################################################'''
 
-class Script_DB_Engine():
+class Excel_Script_DB_Engine(Script_DB_Engine):
     # class variables:
-    script_name: str = None
+    script_path: str = None
+    script_file_name: str = None
+    
 
-    def __init__(self, script_name: str = None):
-        self.script_name = script_name
+    def __init__(self, script_path: str = None, script_file_name: str = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.script_path = script_path
+        self.script_file_name = script_file_name
+
 
     ############################################################
     # getter functions:
 
-    @virtual
     def get_script_names(self) -> List[str]:
         pass
 
-    @virtual
     def get_script_lines(self, script: Script = Script()) -> Union[List[Script], Script]:
         pass
 
-    @virtual
     def get_character_infos(self, character_info: Character_Info = Character_Info()) -> Union[List[Character_Info], Character_Info]:
         pass
+
+
+    ############################################################
+    # excel specific functions:
+
+    def loaf_excel_script(self) -> :
+        
