7045c1f717724f092ec00a08dca4d59661f270bb
Nasenboi <c.boendgen@gmail.com>
2024-05-29 15:47:24 +0200

    UI: work on table

--- front_ends/plugin/vivoac/Source/v_ScriptMenu.cpp
+++ front_ends/plugin/vivoac/Source/v_ScriptMenu.cpp
@@ -14,13 +14,16 @@
 //==============================================================================
 v_ScriptMenu::v_ScriptMenu(VivoacAudioProcessor& p) : v_BaseMenuComponent(p)
 {
+    prevButton.addListener(this);
     addAndMakeVisible(prevButton);
+    nextButton.addListener(this);
     addAndMakeVisible(nextButton);
-
+    scriptTable.getViewport()->setScrollBarsShown(false, false, true, false);
     addAndMakeVisible(scriptTable);
 
     scriptTable.getHeader().addColumn("ID", 1, defaultLength);
     scriptTable.getHeader().addColumn("Script Line", 2, 2 * (defaultLength+margin));
+
 }
 
 v_ScriptMenu::~v_ScriptMenu()
@@ -34,8 +37,41 @@ void v_ScriptMenu::paint (juce::Graphics& g)
 
 void v_ScriptMenu::resized()
 {
-    scriptTable.setBounds(margin, margin, 2 * margin + 3 * defaultLength, getHeight() - (2*margin + defaultHeight));
+    scriptTable.setBounds(margin, margin, 2 * margin + 3 * defaultLength, getHeight() - (3*margin + defaultHeight));
 
     prevButton.setBounds(margin, getHeight() - margin - defaultHeight, defaultLength, defaultHeight);
     nextButton.setBounds(3*margin + 2*defaultLength, getHeight() - margin - defaultHeight, defaultLength, defaultHeight);
 }
+
+void v_ScriptMenu::buttonClicked(juce::Button* button) {
+    if (button == &prevButton) {
+        const juce::SparseSet<int> selection = scriptTable.getSelectedRows();
+        juce::SparseSet<int> newSelection = moveSelection(selection, -1);
+        scriptTable.setSelectedRows(newSelection);
+        scriptTable.scrollToEnsureRowIsOnscreen(newSelection.getRange(0).getStart());
+    }
+    if (button == &nextButton) {
+        const juce::SparseSet<int> selection = scriptTable.getSelectedRows();
+        juce::SparseSet<int> newSelection = moveSelection(selection, 1);
+        scriptTable.setSelectedRows(newSelection);
+        scriptTable.scrollToEnsureRowIsOnscreen(newSelection.getRange(newSelection.getNumRanges() - 1).getStart());
+    }
+}
+
+juce::SparseSet<int> v_ScriptMenu::moveSelection(juce::SparseSet<int> selection, int direction) {
+    juce::SparseSet<int> newSelection;
+    const int numRanges = selection.getNumRanges();
+    const int min = 0, max = scriptTableModel.getScriptLength();
+    juce::Range<int> range;
+    int start, end;
+    for (int i = 0; i < numRanges; ++i) {
+        range = selection.getRange(i);
+        start = range.getStart() + direction;
+        end = range.getEnd() + direction;
+        if (start < min || end > max) { return selection; }
+        range = juce::Range<int>(start, end);
+        newSelection.addRange(range);
+    }
+
+    return newSelection;
+};
--- front_ends/plugin/vivoac/Source/v_ScriptMenu.h
+++ front_ends/plugin/vivoac/Source/v_ScriptMenu.h
@@ -12,6 +12,7 @@
 
 #include <JuceHeader.h>
 #include "v_BaseMenuComponent.h"
+#include "v_Colors.h"
 
 
 //==============================================================================
@@ -24,19 +25,71 @@ public:
     }
 
     void paintRowBackground(juce::Graphics& g, int rowNumber, int width, int height, bool rowIsSelected) override {
+        if (rowIsSelected) {
+            g.fillAll(colors.light_sky_blue);
+        }
     };
 
     void paintCell(juce::Graphics& g, int rowNumber, int columnId, int width, int height, bool rowIsSelected) override {
+        g.setColour(juce::Colours::white);
+        g.setFont(12.0f);
+        juce::String text, id;
+
+        switch (columnId) {
+        case 1:
+            id = juce::String(scriptLines[rowNumber][0]);
+            g.drawText(id, juce::Rectangle(0, 0, width, height), juce::Justification::centred, true);
+            break;
+        case 2:
+            text = juce::String(scriptLines[rowNumber][1]);
+            if (scriptLines[rowNumber][1].length() > 35) { text.append("...", 3); }
+            g.drawText(text, juce::Rectangle(margin, 0, width-2*margin, height), juce::Justification::left, true);
+            break;
+        default:
+            break;
+        };
+    };
+
+    void listWasScrolled() override {
 
     };
+
+    int getScriptLength() {
+        return scriptLines.size();
+    };
+
 private:
-    const int numRows = 15;
+    const int numRows = 20;
+    const v_Colors colors;
+
+    const std::vector<std::array<juce::String, 2>> scriptLines = {
+        {"0", "A cool script line that is also very long and detailed!"},
+        {"1", "A cool script line that is also very long and detailed!"},
+        {"2", "A cool script line that is also very long and detailed!"},
+        {"3", "A cool script line that is also very long and detailed!"},
+        {"4", "A cool script line that is also very long and detailed!"},
+        {"5", "A cool script line that is also very long and detailed!"},
+        {"6", "A cool script line that is also very long and detailed!"},
+        {"7", "A cool script line that is also very long and detailed!"},
+        {"8", "A cool script line that is also very long and detailed!"},
+        {"9", "A cool script line that is also very long and detailed!"},
+        {"10", "A cool script line that is also very long and detailed!"},
+        {"11", "A cool script line that is also very long and detailed!"},
+        {"12", "A cool script line that is also very long and detailed!"},
+        {"13", "A cool script line that is also very long and detailed!"},
+        {"14", "A cool script line that is also very long and detailed!"},
+        {"15", "A cool script line that is also very long and detailed!"},
+        {"16", "A cool script line that is also very long and detailed!"},
+        {"17", "A cool script line that is also very long and detailed!"},
+        {"18", "A cool script line that is also very long and detailed!"},
+        {"19", "A cool script line that is also very long and detailed!"}
+    };
 };
 
 //==============================================================================
 /*
 */
-class v_ScriptMenu  : public v_BaseMenuComponent
+class v_ScriptMenu  : public v_BaseMenuComponent, public juce::Button::Listener
 {
 public:
     v_ScriptMenu(VivoacAudioProcessor& p);
@@ -44,6 +97,11 @@ public:
 
     void paint (juce::Graphics&) override;
     void resized() override;
+
+
+    void buttonClicked(juce::Button* button) override;
+
+    void buttonStateChanged(juce::Button* button) override {};
 
 private:
     // UI Sizes
@@ -54,6 +112,8 @@ private:
     juce::TextButton prevButton{ "<" }, nextButton{ ">" };
     ScriptTableModel scriptTableModel;
     juce::TableListBox scriptTable {"Script", &scriptTableModel };
+
+    juce::SparseSet<int> moveSelection(juce::SparseSet<int> selection, int direction);
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (v_ScriptMenu)
 };
