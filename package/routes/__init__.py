c819eac7a8a58192506b2d68194a2ce835c4c761
Nasenboi <c.boendgen@gmail.com>
2024-08-07 17:32:13 +0200

    remove auth route

--- package/routes/__init__.py
+++ package/routes/__init__.py
@@ -1,6 +1,5 @@
 from .ai_api_handler.routes import AI_API_Handler_Router
 from .audio.routes import Audio_Router
-from .auth.routes import Auth_Router
 from .engine_backend.routes import Engine_Router
 from .script.routes import Script_Router
 from .session.routes import Session_Router
@@ -13,5 +12,4 @@ __all__ = [
     "Session_Router",
     "Engine_Router",
     "User_Router",
-    "Auth_Router",
 ]
