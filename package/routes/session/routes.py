0255e8e02a3326461e5bcba8c95017d19ee9cfad
Nasenboi <c.boendgen@gmail.com>
2024-09-05 10:26:36 +0200

    compare session_id header to path

--- package/routes/session/routes.py
+++ package/routes/session/routes.py
@@ -6,7 +6,7 @@ Imports:
 
 from typing import Annotated
 
-from fastapi import APIRouter, Header, Depends
+from fastapi import APIRouter, Header, HTTPException
 
 from ...utils.functions import fetch_session
 from .functions import *
@@ -36,15 +36,15 @@ class Session_Router(APIRouter):
             methods=["POST"],
         )
         self.add_api_route(
-            path="/get/{session_id}", endpoint=self.get_session_route, methods=["GET"]
+            path="/get/{current_session_id}", endpoint=self.get_session_route, methods=["GET"]
         )
         self.add_api_route(
-            path="/update/{session_id}",
+            path="/update/{current_session_id}",
             endpoint=self.update_session_route,
             methods=["PUT"],
         )
         self.add_api_route(
-            path="/close/{session_id}",
+            path="/close/{current_session_id}",
             endpoint=self.close_session_route,
             methods=["DELETE"],
         )
@@ -54,15 +54,28 @@ class Session_Router(APIRouter):
             self=self, api_engine=self.api_engine, session=session
         )
 
-    async def get_session_route(self, session_id: str) -> Session:
+    async def get_session_route(
+        self,
+        current_session_id: str,
+        session_id: Annotated[str, Header()],
+    ) -> Session:
+        if session_id != current_session_id:
+            raise HTTPException(
+                status_code=403, detail="You are not allowed to access this session!"
+            )
         session = await fetch_session(self.api_engine, session_id)
         return session
 
     async def update_session_route(
         self,
-        session_id: str,
+        current_session_id: str,
+        session_id: Annotated[str, Header()],
         new_session: Session,
     ) -> Session:
+        if session_id != current_session_id:
+            raise HTTPException(
+                status_code=403, detail="You are not allowed to edit this session!"
+            )
         session = await fetch_session(self.api_engine, session_id)
         return await update_session(
             api_engine=self.api_engine,
@@ -71,7 +84,13 @@ class Session_Router(APIRouter):
             session=session,
         )
 
-    async def close_session_route(self, session_id: str) -> Union[str, int]:
+    async def close_session_route(
+        self, current_session_id: str, session_id: Annotated[str, Header()]
+    ) -> Union[str, int]:
+        if session_id != current_session_id:
+            raise HTTPException(
+                status_code=403, detail="You are not allowed to close this session!"
+            )
         return await close_session(
             self=self, api_engine=self.api_engine, session_id=session_id
         )
