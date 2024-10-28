3a0dc231e00294f2eb6c1a2a162df7f2246183cf
Nasenboi <c.boendgen@gmail.com>
2024-10-28 11:10:19 +0100

    valid empty email

--- frontends/reactapp/vivoac-web/types/nullopts.ts
+++ frontends/reactapp/vivoac-web/types/nullopts.ts
@@ -3,6 +3,6 @@ import {z} from 'zod';
 export const nullOptString = z.string().nullable().optional().default(undefined);
 
 export const nullEmail = z.union([
-    z.literal(''),
     z.string().email(),
+    z.string().nullable().optional().default(undefined)
 ]).default(undefined);
